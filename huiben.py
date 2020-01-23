138c0fe70e8a667e062060b23273afda600f7485
dawn2004cn <dawn2004cn@163.com>
2020-01-23 22:58:35 +0800

    "有声绘本"

---
+++ huiben.py
@@ -0,0 +1,191 @@
+import importlib
+import sys
+importlib.reload(sys)
+import requests
+import lxml.etree as etree
+import os
+
+UA_LIST = [
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+    "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+    "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SE 2.X MetaSr 1.0; SE 2.X MetaSr 1.0; .NET CLR 2.0.50727; SE 2.X MetaSr 1.0)",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+    "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+    "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
+]
+class shiwen():
+    # 初始化函数
+    def __init__(self):
+        self.huiben_type ={'guoneihuiben','guowaihuiben','guoxuezaojiao','kewenlangsong','shuiqiangushi','yingwenhuiben'}
+        self.baseUrl = 'https://www.youshenghuiben.com/{}/{}'
+        self.firstUrl = 'https://www.youshenghuiben.com/{}'
+        self.soundUrl = "https://so.gushiwen.org/viewplay.aspx?id={}"
+        self.file_path = f'.//sound//'
+        if os.path.exists(self.file_path) == False:
+            os.mkdir(self.file_path)
+        self.headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0',
+                   'Accept':'*/*'
+                   }
+
+    def __del__(self):
+        print(f'__del__')
+        #self.file.close()
+
+    #每个channel一个文件
+    def open(self,channel):
+        self.file = open(channel+".json", "w",encoding='utf-8')
+
+    #关闭文件
+    def close(self):
+        self.file.close()
+
+    def getHtml(self,url):
+        ''' 爬取网页数据 '''
+        print(f"getHtml...{url}")
+        html = requests.get(url, headers=self.headers)
+        html.encoding = 'utf-8'
+        text = html.text
+        return text
+
+    #解析一页所有诗文数据 html页数据
+    def parseHtml(self,html):
+        if len(html) != 0:
+            try:
+                tree = etree.HTML(html, parser=etree.HTMLParser(encoding='utf-8'))
+                nodes = tree.xpath('//div[@class="border_gray"]/article[@class="archive-list"]')
+                id = 1
+                values = []
+                for node in nodes:
+                    value = {}
+                    imgurl = node.xpath('./figure[@class="thumbnail"]/a/img/@src')
+                    name = node.xpath('./header[@class="entry-header"]/h2[@class="entry-title"]/a/text()')
+                    href = node.xpath('./header[@class="entry-header"]/h2[@class="entry-title"]/a/@href')
+                    updatetime = node.xpath('./div[@class="entry-content"]/span[@class="entry-meta"]/span/text()')
+                    desc = node.xpath('./div[@class="entry-content"]/div[@class="archive-content"]/text()')
+                    tag  = node.xpath('./div[@class="entry-content"]/div[@class="archive-tag"]/a/text()')
+
+                    value['imgurl'] = "".join(imgurl)
+                    value['name'] = "".join(name)
+                    value['href'] = "".join(href)
+                    value['updatetime'] = "".join(updatetime)
+                    value['desc'] = "".join(desc)
+                    value['tag'] = "".join(tag)
+
+                    #下载声音
+                    #value["sound"] = self.getSound(value['href'])
+
+                    imagename = os.path.basename(value['imgurl'])
+                    value["imagename"] = imagename[:-5]
+                    #下载图片
+                    self.download(value['imgurl'],value["imagename"])
+
+                    self.file.write(str(value).encode('utf-8').decode('utf-8'))
+                    self.file.write(f'\n')
+                    values.append(value)
+                return values
+            except:
+                print("fail")
+            return ""
+
+
+    #下载声音
+    def download(self,link,filename):
+        try:
+            pic = requests.get(link,headers=self.headers)
+            if pic.status_code == 200:
+                with open(os.path.join(self.file_path) + os.sep + filename, 'wb') as fp:
+                    fp.write(pic.content)
+                    fp.close()
+            print("下载完成")
+        except Exception as e:
+            print(e)
+
+    #获取声音url
+    def getSound(self,url):
+        html = self.getHtml(url)
+        return self.parseSoundUrl(html)
+
+    def parseSoundUrl(self,html):
+        if len(html) != 0:
+            try:
+                tree = etree.HTML(html, parser=etree.HTMLParser(encoding='utf-8'))
+                nodes = tree.xpath('//div[@class="f1"]/span[@class="huibenzaixian"]')
+                #id = 1
+                value = {}
+                for node in nodes:
+                    audio = node.xpath('./div[@class="jquery_jplayer_1"]/audio/@src')
+                    value['audio'] = "".join(audio)
+                    audioname = os.path.basename(value['audio'])
+                    self.download(value['audio'],audioname)
+                return value['audio']
+            except:
+                print("fail")
+            return ""
+
+    def getTotalPage(self,html):
+        if len(html) != 0:
+            try:
+                tree = etree.HTML(html, parser=etree.HTMLParser(encoding='utf-8'))
+                nodes = tree.xpath('//nav[@class="navigation pagination"]/div[@class="nav-links"]')
+                #id = 1
+                values = []
+                for node in nodes:
+                    value = {}
+                    href = node.xpath('./a[@title="最后一页"]/@href')
+                    value['href'] = "".join(href)
+                    url = value['href']
+                    page = int(url[url.rindex('/')+1:])
+                return page
+            except:
+                print("fail")
+            return ""
+
+
+    def parseAll(self):
+        for value in self.huiben_type:
+            self.open(value)
+            url = self.firstUrl.format(value)
+            html = self.getHtml(url)
+            totalpage = self.getTotalPage(html)
+            for i in range(1,totalpage):
+                huibenurl = self.baseUrl.format(value,i)
+                huibenHtml = self.getHtml(huibenurl)
+                self.parseHtml(huibenHtml)
+            self.close()
+
+if __name__ == '__main__':
+    shiwen = shiwen()
+    shiwen.parseAll()
+    #url = 'https://res.youshenghuiben.com/zb_users/upload/2020/01/huiben_20200114230734_11090.jpeg!lpic'
+    #name =os.path.basename(url)
+    #name = name[:-5]
+    #print(name)
--- sz_house_new.py
+++ sz_house_new.py
@@ -3,6 +3,10 @@ import sys
 importlib.reload(sys)
 import requests
 import lxml.etree as etree
+import pandas as pd
+import openpyxl
+import os
+import shutil
 
 
 #detailUrl = 'http://ris.szpl.gov.cn/bol/'
@@ -26,6 +30,9 @@ class HouseToTxt():
     def closecsv(self):
         self.file.close()
 
+    def copycsv(self,file,file_dir):
+        shutil.copy(file, file_dir)
+
     #根据项目url获取所有楼栋以及branch数据
     def  parseBuilding(self,url):
         #获取项目名称，设置生成文件名为项目名称
@@ -39,7 +46,20 @@ class HouseToTxt():
                 self.parseHouse(branch)
         #关闭文件
         self.closecsv()
+        #csv转换为excel，并去除掉无用列
+        self.csv_to_excel()
 
+    def csv_to_excel(self):
+        csv_dir = "..\\szhouse\\"
+        excel_dir = "..\\szhouse_excel\\"
+        dataframe_dir = "..\\szhouse_excel_1\\"
+        excel_suffix = f".xlsx"
+        self.copycsv(self.building_file,csv_dir)
+        name = self.building_file.split(".")[0]
+        #csv转换为excel
+        self.csv_to_excel_pd(f"{csv_dir}{self.building_file}",f"{excel_dir}{name}{excel_suffix}")
+        #excel去掉无用列，并计算总价
+        self.excel_to_dataFrame(f"{excel_dir}{name}{excel_suffix}",f"{dataframe_dir}{name}{excel_suffix}")
 
     def getBuildingName(self,url):
         html = self.getHouseHtml(url)
@@ -90,25 +110,20 @@ class HouseToTxt():
         nodes = tree.xpath('//div[@id="divShowList"]/tr')
         for node in nodes:
             # print node
-            #node1s = node.xpath('./td')
             node1s = node.xpath('.//div/a')
             for node1 in node1s:
                 # print node1
                 value = {}
                 louceng = node1.xpath('./../preceding-sibling::div[1]/text()')
-                #detail = node1.xpath('./div/a/@href')
                 detail = node1.xpath('./@href')
                 value["louceng"] = "".join(louceng)
                 value["detail"] = "".join(detail)
-                #if len(detail) != 0:
-                    #value["detail"] = "".join(detail)
                 if len(value["detail"]) != 0:
                     value["detail"] = detailUrl + value["detail"]
                     print(value["detail"])
                     text = self.getHouseHtml(value["detail"])
                     detail = value["louceng"]+self.getHouseDetail(text) + '\n'
                     self.file.write(detail.encode('utf-8').decode('utf-8'))
-                    # print value["louceng"],value["detail"]
 
     def getHouseDetail(self,text):
         tree = etree.HTML(text, parser=etree.HTMLParser(encoding='utf-8'))
@@ -125,6 +140,69 @@ class HouseToTxt():
         detail = detail.replace(u'\r\n', '')
         detail = detail.replace(u'\a', '')
         return detail
+
+    #利用pandas将csv转换为excel文件
+    def csv_to_excel_pd(self,src_file,dst_file):
+            csv = pd.read_csv(src_file, encoding='utf-8')
+            csv.to_excel(dst_file, sheet_name='data')
+
+    #将csv目录的csv文件转换为excel文件
+    def csv_to_excel_pd_dir(self):
+        csv_dir = ''
+        excel_dir = ''
+        dataframe_dir = ''
+        if (csv_dir == ''):
+            csv_dir = "..\\szhouse\\"
+        if (excel_dir == ''):
+            excel_dir = "..\\szhouse_excel\\"
+        if (dataframe_dir == ''):
+            dataframe_dir = "..\\szhouse_excel_1\\"
+        all_scv_file = []
+        all_file = os.listdir(csv_dir)
+        for filename in all_file:
+            if ".csv" in filename:
+                all_scv_file.append(filename)
+        all_scv_file.sort()
+        i = 0
+        csv_file_num = len(all_scv_file)
+        print(f"当前共有{csv_file_num}个csv文件需要转换，即将进行处理请稍等...")
+        # 此层for循环是逐个csv文件进行处理
+        for csv_file_name in all_scv_file:
+            #self.csv_to_excel_pd(csv_file_name,)
+            name = csv_file_name.split(".")[0]
+            excel_suffix = f".xlsx"
+            input_file_csv_path = f"{csv_dir}{csv_file_name}"
+            out_file_excel_name = f"{excel_dir}{name}{excel_suffix}"
+            dataframe_file_excel_name = f"{dataframe_dir}{name}{excel_suffix}"
+            self.csv_to_excel_pd(input_file_csv_path,out_file_excel_name)
+            self.excel_to_dataFrame(out_file_excel_name,dataframe_file_excel_name)
+
+    def excel_to_dataFrame(self,src_file,dst_file):
+        df = pd.DataFrame(pd.read_excel(src_file))
+        #print(df)
+        df.columns = list('abcdefghigklmnopqrstuvwxyz12')
+        #取有用的列
+        df1 = df.iloc[:,lambda df:[3,5,7,9,11,13,15,17,19,21]]
+        df1.columns = [f"项目楼栋情况",f"座号",f"合同号",f"拟售价格(元/平方米)",f"楼层",f"房号",f"用途",f"建筑面积(平方米)",f"户内面积(平方米)",f"分摊面积(平方米)"]
+        print(df1)
+        #拟售价格去掉单位
+        df1["拟售价格(元/平方米)"] = df1["拟售价格(元/平方米)"].str.replace("元/平方米","")
+        df1["拟售价格(元/平方米)"] = df1["拟售价格(元/平方米)"].str.replace("按建筑面积计","")
+        df1["拟售价格(元/平方米)"] = df1["拟售价格(元/平方米)"].str.replace("(","")
+        df1["拟售价格(元/平方米)"] = df1["拟售价格(元/平方米)"].str.replace(")","")
+        #已售出的价格替换成0
+        df1["拟售价格(元/平方米)"] = df1["拟售价格(元/平方米)"].str.replace("--","0")
+        #面积全部去掉单位
+        df1["建筑面积(平方米)"] = df1["建筑面积(平方米)"].str.replace("平方米","")
+        df1["户内面积(平方米)"] = df1["户内面积(平方米)"].str.replace("平方米","")
+        df1["分摊面积(平方米)"] = df1["分摊面积(平方米)"].str.replace("平方米","")
+        #计算总价
+        df1["总价(元)"] = df1.apply(lambda x: round(float(x['拟售价格(元/平方米)']) * float(x['建筑面积(平方米)']),2), axis=1)
+        df1["总价(万元)"] = df1.apply(lambda x: round(float(x['拟售价格(元/平方米)']) * float(x['建筑面积(平方米)'])/10000,2), axis=1)
+        df1["总价*98折(万元)"] = df1.apply(lambda x: round(float(x['拟售价格(元/平方米)']) * float(x['建筑面积(平方米)'])*0.98/10000,2), axis=1)
+        df1["使用率"] = df1.apply(lambda x:'{:.2%}'.format(float(x['户内面积(平方米)'])/float(x['建筑面积(平方米)'])),axis=1)
+        print(df1)
+        df1.to_excel(dst_file)
 
 #此处从http://zjj.sz.gov.cn/ris/bol/szfdc/index.aspx 中查找到要爬取的楼盘地址。
 bihai1 = 'http://ris.szpl.gov.cn/bol/building.aspx?id=32387&presellid=36452'
@@ -166,8 +244,12 @@ jiulongtai='http://zjj.sz.gov.cn/ris/bol/szfdc/projectdetail.aspx?id=42725'
 beiyuehui='http://zjj.sz.gov.cn/ris/bol/szfdc/projectdetail.aspx?id=42993'
 dehongtianxia='http://zjj.sz.gov.cn/ris/bol/szfdc/projectdetail.aspx?id=42953'
 furunleting='http://zjj.sz.gov.cn/ris/bol/szfdc/projectdetail.aspx?id=42934'
+yian='http://zjj.sz.gov.cn/ris/bol/szfdc/projectdetail.aspx?id=43033'
+
+
 if __name__ == '__main__':
     house = HouseToTxt()
-    house.parseBuilding(beiyuehui)
-    house.parseBuilding(dehongtianxia)
-    house.parseBuilding(furunleting)
+    house.parseBuilding(huihuang)
+    #house.csv_to_excel_pd_dir()
+    #house.parseBuilding(dehongtianxia)
+    #house.parseBuilding(furunleting)
